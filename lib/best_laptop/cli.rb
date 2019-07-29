fe5e9f1973db13b82fe1ad5566c9506648ad2119
Huy Do <huyddo40@gmail.com>
2019-07-29 16:11:30 +0000

    refactor cli.rb

--- lib/best_laptop/cli.rb
+++ lib/best_laptop/cli.rb
@@ -1,9 +1,5 @@
-# require_relative "./scraper.rb"
-# require_relative './laptop.rb'
-
 class BestLaptop::CLI
-# class CLI 
-  
+
   def run 
     make_laptops
     list_laptops
@@ -11,16 +7,12 @@ class BestLaptop::CLI
   end
   
   def make_laptops
-    # laptops_arr = Scraper.scrape_laptops
-    # Laptop.create(laptops_arr)
-    
     laptops_arr = BestLaptop::Scraper.scrape_laptops
     BestLaptop::Laptop.create(laptops_arr)
   end
   
   def list_laptops
     puts "The Best Laptops for 2019."
-    # @list = Laptop.all
     @list = BestLaptop::Laptop.all
     @list.each.with_index(1) do |laptop, i|
       puts "#{i}. #{laptop.name}"
@@ -28,14 +20,9 @@ class BestLaptop::CLI
   end
   
   def add_laptop_detail(url,index)
-    # Laptop.all.each do |laptop|
-      # laptop = Laptop.all[index]
-      # info = Scraper.scrape_laptop_page(url)
-      
-      laptop = BestLaptop::Laptop.all[index]
-      info = BestLaptop::Scraper.scrape_laptop_page(url)
-      laptop.add_laptop_info(info)
-    # end
+    laptop = BestLaptop::Laptop.all[index]
+    info = BestLaptop::Scraper.scrape_laptop_page(url)
+    laptop.add_laptop_info(info)
   end
   
   def menu
@@ -58,10 +45,6 @@ class BestLaptop::CLI
         answer = gets.strip
     
         if ["Y", "YES"].include?(answer.upcase)
-          # content = scrape_laptop_page(laptop_url)
-          # content = Laptop.content
-          # puts content
-          
           add_laptop_detail(laptop.url,input.to_i - 1)
           puts
           puts "Pros: #{laptop.pros}"
@@ -70,52 +53,14 @@ class BestLaptop::CLI
           puts
           puts "Bottom Line: #{laptop.bottom_line}"
           puts
-        # else
-        #   puts "Would you like to exit or show the list again?"
-        #   input = gets.strip
         end
         
-        # selected_laptop(input.to_i)
-        # laptop = @list[input.to_i - 1]
-        # puts "Name: #{laptop.name} - #{laptop.price}"
-        # puts "Review: #{laptop.description}"
-        # puts "  Read Review: #{laptop.url}"
       elsif input == "show"
         list_laptops
-      else
-        puts "Please check your input."
+      elsif input != 'exit'
+        puts "Would you like to exit or show the list again?"
       end
     end
-    
-   
-    # Laptop.all.each do |laptop|
-    #   puts "Name: #{laptop.name}"
-    #   puts "  #{laptop.price}"
-    #   puts "  Description: #{laptop.description}"
-    #   puts "  Read Review: #{laptop.url}"
-    # end
-  end
-  
-  def selected_laptop(number)
-    laptop = @list[number - 1]
-    input = nil
-    
-    puts "Name: #{laptop.name} - #{laptop.price}"
-    puts "Description: #{laptop.description}"
-    puts "Would you like to read more?"
-    input = gets.strip
-    
-    if ["Y", "YES"].include?(input.upcase)
-      # content = scrape_laptop_page(laptop_url)
-      # content = @list.content
-      # puts content
-      # puts "  Read Review: #{laptop.url}"
-      puts "Pros: #{laptop.pros}"
-      puts "Cons: #{laptop.pros}"
-      puts "Bottom Line: #{laptop.pros}"
-    end
-    puts "Would you like to exit or show the list again?"
-    input = gets.strip
   end
   
 end
