a9ba967a4de31d5778d3d6067a0a06dceb103696
HuydDo <huydinhdo@yahoo.com>
2019-10-04 21:43:00 -0600

    update best_laptop.rb

--- lib/best_laptop.rb
+++ lib/best_laptop.rb
@@ -6,10 +6,10 @@ module BestLaptop
 end
 
 require 'pry'
-require 'nokogiri'
+require 'nokogiri', '>= 1.10.4'
 require 'open-uri'
 require 'colorize'
-
+# gem "nokogiri", ">= 1.10.4"
 require_relative "./best_laptop/scraper"
 require_relative "./best_laptop/laptop"
 require_relative "./best_laptop/version"
